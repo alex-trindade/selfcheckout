d05ee41ff41aa769a600260c048cef272946e010
alex-trindade <alextrindade.com.br@gmail.com>
2025-02-18 22:26:37 -0300

    feat shadcn

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import {Button} from "@/components/ui/button"
+import {Input} from "@/components/ui/input"
+
+const ProductsPage = ()=> {
+    return (
+        <div className="p-5 border border-red-500 rounded-xl">
+            <h1 className="text-red-500">Product Page</h1>
+            <Button>Teste</Button>
+            <Input placeholder="Vamos codar"></Input>
+        </div>
+    )
+}
+
+export default ProductsPage;
